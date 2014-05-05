97cf5ee6bcfe8430029bc0009f3a0d73fe359edb
avil13 <13.avil@gmail.com>
2014-05-05 20:59:39 +0400

    Second page

--- src/js/app.js
+++ src/js/app.js
@@ -4,19 +4,19 @@ xApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider
             .when('/spending', {
-                templateUrl: 'pages/page_1.html',
+                templateUrl: 'pages/page_spending.html',
                 controller: 'spendingCtrl'
             })
             .when('/profit', {
-                templateUrl: 'pages/page_2.html',
-                controller: 'BodyCtrl'
-            })
-            .when('/plans', {
                 templateUrl: 'pages/page_1.html',
                 controller: 'BodyCtrl'
             })
+            .when('/plans', {
+                templateUrl: 'pages/page_plans.html',
+                controller: 'plansCtrl'
+            })
             .when('/graphik', {
-                templateUrl: 'pages/page_2.html',
+                templateUrl: 'pages/page_1.html',
                 controller: 'BodyCtrl'
             })
             .otherwise({
--- src/js/controllers.js
+++ src/js/controllers.js
@@ -18,6 +18,7 @@ xApp.controller('BodyCtrl', function($scope) {
 });
 
 
+
 xApp.controller('menuCtrl', ['$scope', '$location', '$route',
     function($scope, $location, $route) {
 
@@ -49,6 +50,13 @@ xApp.controller('menuCtrl', ['$scope', '$location', '$route',
 xApp.controller('spendingCtrl', ['$scope', '$http',
     function($scope, $http) {
         $http.get('groups.json').success(function(data) {
+
+            if (!br.storage.get('plans')) {
+                br.storage.set('plans', []);
+            }
+
+            data = data.concat(br.storage.get('plans'));
+
             $scope.groups = data;
             $scope.select = $scope.groups[0];
         });
@@ -64,15 +72,34 @@ xApp.controller('spendingCtrl', ['$scope', '$http',
                 return false;
             }
 
+
+            var plans = br.storage.get('plans');
+            var select =  JSON.stringify( $scope.select);
+            var i_plans = false;
+
+            for(var i=0; i< plans.length; i++){
+                if(JSON.stringify(plans[i])===select){
+                    i_plans = i;
+                }
+            }
+
+            if(i_plans!==false){
+                plans[i_plans]['money'] = plans[i_plans]['money'] + $scope.money;
+                br.storage.set('plans', plans);
+            }
+
+
             var flow = {};
 
             flow[parseInt(($scope.spending_date.getTime() / 1000), 10) + ''] = {
                 date: $scope.spending_date,
                 money: $scope.money,
+                nead: ($scope.select.nead || ''),
                 category: $scope.select.value,
                 name: $scope.select.name,
                 comment: $scope.comment
             };
+
 
             $scope.spending_date = new Date();
 
@@ -85,9 +112,62 @@ xApp.controller('spendingCtrl', ['$scope', '$http',
             $scope.money = '';
             $scope.comment = '';
 
-            console.log(br.storage.get('flow'));
         };
 
+    }
+]);
+
+
+
+xApp.controller('plansCtrl', ['$scope',
+    function($scope) {
+        $scope.new_plan = false;
+
+        if (!br.storage.get('plans')) {
+            br.storage.set('plans', []);
+        }
+
+        $scope.plans = br.storage.get('plans');
+
+
+
+        $scope.savePlan = function() {
+            if ($scope.name === undefined || $scope.name === '') {
+                alert('Вы забыли указать план');
+                return false;
+            }
+
+            if ($scope.nead === undefined || parseInt($scope.nead, 10) <= 0) {
+                alert('Сумма указана неверно');
+                return false;
+            }
+
+
+            var plan = {
+                grp: 'Планы',
+                money: 0,
+                name: $scope.name,
+                nead: $scope.nead
+            };
+
+            $scope.plans = br.storage.append('plans', plan).get('plans');
+
+            $scope.name = '';
+            $scope.nead = '';
+            $scope.new_plan = false;
+
+        };
+
+
+
+        $scope.removePlan = function(name) {
+            if (confirm("Вы уверены, что вам больше не нужен\n" + name.name)) {
+                var i = $scope.plans.indexOf(name);
+                $scope.plans.splice(i, 1);
+                br.storage.set('plans', $scope.plans);
+            }
+
+        };
     }
 ]);
 
