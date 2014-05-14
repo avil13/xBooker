70c0d8341b0f2b0393a3ba383ceb5e65c1e8d975
avil13 <a.1.3@mail.ru>
2014-05-14 14:42:14 +0400

    creating zip builder on node

--- archive-creator.js
+++ archive-creator.js
@@ -1,26 +1,86 @@
 var file_system = require('fs');
 var archiver = require('archiver');
 
-// TODO: change the index file function
+
+var Comment = function(file) {
+
+    if (!file)
+        file = 'index.html';
 
 
-var output = file_system.createWriteStream('Src.zip');
-var archive = archiver('zip');
+    file_system.readFile(file, 'utf8', function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
 
-output.on('close', function () {
-    console.log(archive.pointer() + ' total bytes');
-    console.log('archiver has been finalized and the output file descriptor has closed.');
-});
+        var line_data = data.split("\n");
 
-archive.on('error', function(err){
-    throw err;
-});
+        for (var i = 0; i < line_data.length; i++) {
 
-archive.pipe(output);
+            if (line_data[i].split('<!-- !@@').length > 1 || line_data[i].split('@@! -->').length > 1) {
 
-archive.bulk([
-  { src: ['src/**'], data: { date: new Date() } },
-  { expand: true, cwd: 'mydir', src: ['**'], dest: 'newdir' }
-]);
+                line_data[i] = line_data[i].split('<!-- !@@').join('<!-- !@ -->').split('@@! -->').join('<!-- @! -->');
 
-archive.finalize();
+            } else if (line_data[i].split('<!-- !@ -->').length > 1 || line_data[i].split('<!-- @! -->').length > 1) {
+
+                line_data[i] = line_data[i].split('<!-- !@ -->').join('<!-- !@@').split('<!-- @! -->').join('@@! -->');
+
+            }
+        }
+
+        data = line_data.join("\n");
+
+        // write file
+        file_system.writeFile(file, data, function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("The file was saved!");
+            }
+        });
+    });
+};
+
+
+
+Comment('src/index.html');
+
+
+setTimeout(function() {
+
+
+    var output = file_system.createWriteStream('Src.zip');
+    var archive = archiver('zip');
+
+    output.on('close', function() {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+    });
+
+    archive.on('error', function(err) {
+        throw err;
+    });
+
+    archive.pipe(output);
+
+    archive.bulk([{
+        src: ['src/**'],
+        data: {
+            date: new Date()
+        }
+    }, {
+        expand: true,
+        cwd: 'mydir',
+        src: ['**'],
+        dest: 'newdir'
+    }]);
+
+    archive.finalize();
+
+}, 300);
+
+setTimeout(function () {
+    Comment('src/index.html');
+}, 1000);
+
+
